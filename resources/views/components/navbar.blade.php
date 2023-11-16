63089557f0e90932a4568dbe3489bf07cc5d44d7
Ahmed Znagui <amed.bob88@gmail.com>
2023-11-16 16:24:05 +0100

    start project

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Photos</a></li>
+      <li><a href="#">Videos</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
